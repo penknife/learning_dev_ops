bb12390b73f177d979e704843ee3cb8a0ad8c8c7
nikolay.belyaev <nbelyaev@deloitte.com>
2019-10-08 14:10:47 +0300

    fixes

--- homework4/homework4.groovy
+++ homework4/homework4.groovy
@@ -1,14 +1,14 @@
 def project = "learning_dev_ops"
 job('homework4:docker ngnix') {
     scm {
-        github("penknife/${project}", $BRANCH, "https")
+        github("penknife/${project}", "master", "https")
     }
     wrappers {
         preBuildCleanup()
     }
     steps {
         def build_docker_image = '''#!/bin/bash +x
-        cd "${project}/homework4"
+        cd "$project/homework4"
         docker build -t new-nginx . 
         docker run --name docker-nginx -d new-nginx
         '''.stripIndent()
